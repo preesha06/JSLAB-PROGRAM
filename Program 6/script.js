26217282bff39189837195c43c97cf63975e365d
preesha06 <24bcs140@kgcas.com>
2025-09-18 20:23:49 +0530

    Add files via upload

---
+++ Program 6/script.js
@@ -0,0 +1,28 @@
+function addTask() {
+
+    const taskText = document.getElementById("taskInput").value.trim();
+
+    if(taskText==="") return;
+
+        const li = document.createElement("li");
+        li.textContent  = taskText;
+        
+        li.addEventListener("click",function() {
+            li.classList.toggle("completed");
+        })
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent  = "Remove";
+        removeBtn.className = "remove";
+
+        removeBtn.addEventListener("click",function(e) {
+            e.stopPropagation();
+            li.remove();
+        })
+
+        li.appendChild(removeBtn);
+        document.getElementById("taskList").appendChild(li);
+        document.getElementById("taskInput").value=""; 
+
+}
+    
